9207524ab7bc228663b78bdbc8d2fdaf6f388b02
Yaswanth648 <81416962+Yaswanth648@users.noreply.github.com>
2021-09-23 21:56:54 +0530

    Update progress.py

--- progress.py
+++ progress.py
@@ -28,7 +28,7 @@ async def progress_for_pyrogram(
             ''.join(["◇" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "<b>☑️ Completed:</b> {0} \n<b>🗄️ Total Size:</b> {1}\n<b>🚄 Speed:</b> {2}/s\n<b>🕰️ ETA:</b> {3}\n<b></b>\n\n<b>⌊`♟️ Dlaize`⌉</b>\n".format(
+        tmp = progress + "<b>☑️ Completed:</b> {0} \n<b>🗄️ Total Size:</b> {1}\n<b>🚄 Speed:</b> {2}/s\n<b>🕰️ ETA:</b> {3}\n<b></b>\n\n<b>⌊`♟️ Rakesh`⌉</b>\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
